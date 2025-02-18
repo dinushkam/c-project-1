8cde500e981236ed914985798e24afa2b6386506
Dinushka Malshan <148640575+dinushkam@users.noreply.github.com>
2025-02-18 15:14:17 +0530

    Add files via upload

---
+++ c# project 1/c# project 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c__project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number: ");
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+
+            int divisors = 0;
+
+            for(int i= 1; i<= number; i++)
+            {
+                if(number % i == 0)
+                {
+                    divisors++;
+
+                }
+            }
+            if(divisors == 2)
+            {
+                Console.Write("this is prime number");
+            }
+            else
+            {
+                Console.Write("this is not a prime number");
+            }
+
+
+
+        }
+    }
+}
